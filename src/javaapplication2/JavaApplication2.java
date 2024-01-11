395ce46449d4e87b754a1401e1e547935ca3e695
csc340sp24 <csc340sp24@gmail.com>
2024-01-11 08:50:39 -0500

    initial commit

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,17 @@
+
+package javaapplication2;
+
+/**
+ *
+ * @author sentini
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
